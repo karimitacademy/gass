533cb7f206fbd9477e6f0c11c7b98549297ad0b6
karimitacademy <nkavkazka123@gmail.com>
2022-12-27 11:10:47 +0600

    message

---
+++ assets/script/script.js
@@ -0,0 +1,60 @@
+const inp = document.querySelector('.guess__inp')
+const btn = document.querySelector('.guess__btn')
+const previous = document.querySelector('.guess__previous')
+const answer = document.querySelector('.guess__answer')
+const restartBtn = document.querySelector('.guess__restart')
+
+let attempt = 5
+
+const randomNumber = Math.round(Math.random() * 100)
+console.log(randomNumber)
+
+btn.addEventListener('click', () => {
+  if (attempt) {
+    if (randomNumber === +inp.value) {
+      answer.innerText = 'Вы победили!'
+      restartBtn.style.display = 'block'
+      btn.disabled = true
+    } else if (randomNumber > +inp.value) {
+      answer.innerText = 'Загаданное число меньше'
+    } else {
+      answer.innerText = 'Загаданное число больше'
+    }
+    previous.innerText += ` ${inp.value} `
+    attempt--
+  } else {
+    answer.innerText = `Game over! Загаданное число - ${randomNumber}`
+    restartBtn.style.display = 'block'
+    restartBtn.style.color = 'while'
+    btn.disabled = true
+  }
+})
+
+restartBtn.addEventListener('click', () => {
+  window.location.reload();
+})
+
+const h1 = document.querySelector('h1')
+
+const color = ['red', 'blue', 'green']
+let i = 1
+
+setInterval(() => {
+  h1.style.backgroundColor = color[i++]
+  if (i === 3) {
+  i = 0
+  }
+}, 500)
+
+
+
+
+
+
+
+
+
+// const a = '5'
+// console.log(typeof +a)
+// console.log(typeof Number (a))
+// console.log(typeof a)
